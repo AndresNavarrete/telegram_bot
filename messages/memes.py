187b712c1a0abea014196d9f9f8c42861cc86c78
Andrés Navarrete <ainavarrete@uc.cl>
2022-05-31 22:44:49 -0400

    Added memes

--- messages/memes.py
+++ messages/memes.py
@@ -1,9 +1,12 @@
 import requests
 import json
-
+import random
 
 def get_meme_url():
-    url = "https://meme-api.herokuapp.com/gimme"
+
+    subreddits = ["memes", "dankmemes", "me_irl", "wholesomememes", "PrequelMemes"]
+    sub = random.choice(subreddits)
+    url = "https://meme-api.herokuapp.com/gimme/" + sub
     response = requests.get(url).text
     data = json.loads(response)
     meme_url = data.get('url')
